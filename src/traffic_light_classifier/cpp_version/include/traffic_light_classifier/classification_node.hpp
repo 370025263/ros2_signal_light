3bf3970c6f2cc2a78427588653d6dcff8a432baf
stone meng <370025263@qq.com>
2024-09-19 09:45:41 +0800

    add: 添加相机内参矩阵

--- src/traffic_light_classifier/cpp_version/include/traffic_light_classifier/classification_node.hpp
+++ src/traffic_light_classifier/cpp_version/include/traffic_light_classifier/classification_node.hpp
@@ -6,9 +6,11 @@
 #include <std_msgs/msg/string.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include "traffic_light_msg/msg/traffic_light_msg.hpp"
+#include <autoware_perception_msgs/msg/traffic_light_group_array.hpp>
 
-#include <torch/script.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+
 #include <map>
 
 namespace traffic_light_classifier
@@ -23,14 +25,18 @@ private:
   void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg);
   void debugCallback(const traffic_light_msg::msg::TrafficLightMsg::SharedPtr msg);
   void processImage(const cv::Mat& cv_image, const std::string& true_label = "");
-  cv::Mat drawResult(const cv::Mat& image, const std::string& label);
+  cv::Mat drawResult(const cv::Mat& image, const std::vector<cv::Rect>& boxes, const std::vector<std::string>& labels);
 
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
   rclcpp::Subscription<traffic_light_msg::msg::TrafficLightMsg>::SharedPtr debug_subscription_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr result_publisher_;
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_result_publisher_;
+  rclcpp::Publisher<autoware_perception_msgs::msg::TrafficLightGroupArray>::SharedPtr traffic_signals_publisher_;
 
-  torch::jit::script::Module model_;
+  // 使用 OpenCV DNN 模块加载 ONNX 模型
+  cv::dnn::Net detection_net_;
+  cv::dnn::Net classification_net_;
+
   std::map<std::string, int> class_to_idx_;
   std::map<int, std::string> idx_to_class_;
   
@@ -38,6 +44,9 @@ private:
   int correct_predictions_;
   double total_processing_time_;
   int total_processed_images_;
+
+  // 假设的相机内参矩阵，用于将像素坐标转换为世界坐标（根据您的相机参数进行设置）
+  cv::Mat camera_intrinsic_matrix_;
 };
 
 }  // namespace traffic_light_classifier
