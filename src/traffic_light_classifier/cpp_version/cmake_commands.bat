dd161b373293c6f91f3a94362602be18016f472c
370025263 <370025263@qq.com>
2024-09-20 17:37:42 +0800

    add: clion

---
+++ src/traffic_light_classifier/cpp_version/cmake_commands.bat
@@ -0,0 +1,3 @@
+/usr/bin/cmake /home/casia/jupyter_notebooks/ros_ws/src/traffic_light_classifier/cpp_version -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  -DCMAKE_INSTALL_PREFIX=/home/casia/jupyter_notebooks/ros_ws/src/traffic_light_classifier/cpp_version
+/usr/bin/cmake --build /home/casia/jupyter_notebooks/ros_ws/src/traffic_light_classifier/cpp_version -- -j8 -l8
+/usr/bin/cmake --install /home/casia/jupyter_notebooks/ros_ws/src/traffic_light_classifier/cpp_version
